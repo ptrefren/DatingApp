72eb0ceddc488f8cdc794bc399ab03822ef9c634
ptrefren <trefrenpeter@comcast.net>
2023-10-19 16:50:40 -0700

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -21,10 +21,7 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new ();
-
- //   public int GetAge()
- //   {
- //       return DateOfBirth.CalculateAge();
- //   }
-
+    public List<UserLike> LikedByUsers { get; set; }
+    public List<UserLike> LikedUsers { get; set; }
+ 
 }
